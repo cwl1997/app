12a02e069551ca918fd091db3fa28a32e0352091
cwl1997 <928024702@qq.com>
2020-12-28 19:32:12 +0800

    同步

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+   <div class="container">
+
+   </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
+<style lang="scss" scoped>
+.container{
+  width: 50vw;
+  height: 300px;
+  background: #000;
+}
+</style>
